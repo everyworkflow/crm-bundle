e67161a752d5871c6a24cb34c7e97698e03e0f87
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:04:18 +0000

    Added ?ExtensionInterface

--- EveryWorkflowCrmBundle.php
+++ EveryWorkflowCrmBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\CrmBundle;
 
 use EveryWorkflow\CrmBundle\DependencyInjection\CrmExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowCrmBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new CrmExtension();
     }
